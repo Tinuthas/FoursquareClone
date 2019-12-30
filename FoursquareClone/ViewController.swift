506c2e607a49d46c47bee7f2225d0909cef2e27f
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-29 22:29:16 -0300

    retrieving data

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -13,15 +13,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        /*
         let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "Apple"
-        parseObject["calories"] = 100
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
         parseObject.saveInBackground { (success, error) in
             if error != nil {
                 print(error?.localizedDescription ?? "Error")
             }else{
                 print("uploaded")
+            }
+        }*/
+        
+        let query = PFQuery(className: "Fruits")
+        query.whereKey("calories", greaterThan: 120)
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else {
+                print(objects)
             }
         }
     }
