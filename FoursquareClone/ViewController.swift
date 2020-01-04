23362f3b8f3a68df57c35454acef13d9bc5dc22c
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-04 13:34:15 -0300

    sign up

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -11,31 +11,66 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "Banana"
-        parseObject["calories"] = 150
-        parseObject.saveInBackground { (success, error) in
-            if error != nil {
-                print(error?.localizedDescription ?? "Error")
-            }else{
-                print("uploaded")
-            }
-        }*/
         
-        let query = PFQuery(className: "Fruits")
-        query.whereKey("calories", greaterThan: 120)
-        query.findObjectsInBackground { (objects, error) in
-            if error != nil {
-                print(error?.localizedDescription)
-            }else {
-                print(objects)
+        
+    }
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        if usernameText.text != "" && passwordText.text != "" {
+            let user = PFUser()
+            user.username = usernameText.text!
+            user.password = passwordText.text!
+            
+            user.signUpInBackground { (success, error) in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error!!")
+                }else{
+                    print("OK!")
+                }
             }
+        }else {
+            makeAlert(title: "Error", message: "Username / Password??")
         }
     }
-
+    
+    func makeAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let addButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(addButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    let parseObject = PFObject(className: "Fruits")
+    parseObject["name"] = "Banana"
+    parseObject["calories"] = 150
+    parseObject.saveInBackground { (success, error) in
+        if error != nil {
+            print(error?.localizedDescription ?? "Error")
+        }else{
+            print("uploaded")
+        }
+    }
+    
+    let query = PFQuery(className: "Fruits")
+    query.whereKey("calories", greaterThan: 120)
+    query.findObjectsInBackground { (objects, error) in
+        if error != nil {
+            print(error?.localizedDescription)
+        }else {
+            print(objects)
+        }
+    }*/
+    
 
 }
 
