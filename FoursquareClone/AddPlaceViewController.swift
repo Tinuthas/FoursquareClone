d472490b713c30652338ecfc9f17aa572538bac9
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-07 22:49:47 -0300

    add view place controller

---
+++ FoursquareClone/AddPlaceViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddPlaceViewController.swift
+//  FoursquareClone
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 07/01/20.
+//  Copyright © 2020 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class AddPlaceViewController: UIViewController {
+    @IBOutlet weak var placeNameText: UITextField!
+    @IBOutlet weak var placeTypeText: UITextField!
+    @IBOutlet weak var placeAtmosfereText: UITextField!
+    @IBOutlet weak var placeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickedNext(_ sender: Any) {
+    }
+    
+
+}
