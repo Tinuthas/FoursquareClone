fab32cd905a23ee93ca1509ba61ea0664cbefbf5
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-29 00:17:35 -0300

    create details view controller

---
+++ FoursquareClone/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsViewController.swift
+//  FoursquareClone
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 29/01/20.
+//  Copyright © 2020 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
